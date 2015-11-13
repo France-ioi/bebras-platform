196b51f83c4abcfbafb40065d255d6a3df21b21e
Elie Roux <elie.roux@telecom-bretagne.eu>
2015-11-13 13:26:19 +0100

    really log questionKey (file forgotten in last commit)

--- contestInterface/logError.php
+++ contestInterface/logError.php
@@ -9,6 +9,7 @@ require_once("../shared/common.php");
 initSession();
 
 $teamID = isset($_SESSION["teamID"]) ? $_SESSION["teamID"] : null;
+$questionKey = isset($_POST["questionKey"]) ? $_POST["questionKey"] : null;
 
 $errormsg = $_POST['errormsg'];
 
@@ -18,6 +19,6 @@ $browser = $bc->getBrowser();
 
 $browserStr = $browser->browser.' '.$browser->version.' ('.$browser->platform.')';
 
-$stmt = $db->prepare('insert into error_log (date, teamID, message, browser) values (NOW(), :teamID, :errormsg, :browserStr);');
-$stmt->execute(['teamID' => $teamID, 'errormsg' => $errormsg, 'browserStr' => $browserStr]);
+$stmt = $db->prepare('insert into error_log (date, teamID, message, browser, questionKey) values (NOW(), :teamID, :errormsg, :browserStr, :questionKey);');
+$stmt->execute(['teamID' => $teamID, 'errormsg' => $errormsg, 'browserStr' => $browserStr, 'questionKey' => $questionKey]);
 echo json_encode(['success' => true]);
