3e4792a5e743e4d3f1202d7c6a267bafa6239f05
Elie Roux <elie.roux@telecom-bretagne.eu>
2015-11-13 08:04:03 +0100

    fix getAllowedDomains

--- teacherInterface/domains.php
+++ teacherInterface/domains.php
@@ -5,7 +5,8 @@ function getAllowedDomains() {
 
    $filename = __DIR__.'/regions/'.$config->teacherInterface->countryCode.'/domains.json';
    if (is_readable($filename)) {
-      return json_decode(file_get_contents($filename));
+   	  $domains = json_decode(file_get_contents($filename));
+   	  return $domains->domains;
    }
    else {
       return array();
