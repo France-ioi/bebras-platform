576bd8c2c2914f689189cbb03d8ecd7913ce2d6b
Elie Roux <elie.roux@telecom-bretagne.eu>
2015-12-10 14:56:31 +0100

    fix award_threshold table

--- dbv_data/revisions/1197/award_threshold.sql
+++ dbv_data/revisions/1197/award_threshold.sql
@@ -1,10 +1,15 @@
+
 CREATE TABLE IF NOT EXISTS `award_threshold` (
-  `ID` bigint(20) NOT NULL,
+`ID` bigint(20) NOT NULL,
   `contestID` bigint(20) NOT NULL,
   `gradeID` bigint(20) NOT NULL,
   `awardID` bigint(20) NOT NULL,
-  `minimalScore` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `nbContestants` int(11) NOT NULL,
+  `minScore` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 ALTER TABLE `award_threshold`
- ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `contestGradeAward` (`contestID`,`gradeID`,`awardID`);
+ ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `all` (`contestID`,`gradeID`,`awardID`,`nbContestants`);
+
+ALTER TABLE `award_threshold`
+MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
