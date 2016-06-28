871ebea699a4f33190f36f1ddee8e2097255fa7f
robert <blog+github@zgtm.de>
2016-06-29 00:50:50 +0200

    Set session timeout to 30 seconds

--- teacherInterface/commonAdmin.php
+++ teacherInterface/commonAdmin.php
@@ -3,6 +3,11 @@
 
 require_once("../shared/common.php");
 
+// server should keep session data for at least 30 secs
+ini_set('session.gc_maxlifetime', 30);
+// client should remember their session id for 30 secs
+session_set_cookie_params(30);
+
 session_name('coordinateur2');
 session_start();
 
