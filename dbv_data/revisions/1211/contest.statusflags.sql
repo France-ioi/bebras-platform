d7146f375bfe907a8493f3c1c1aec86d920ff0d4
robert <blog+github@zgtm.de>
2016-07-14 16:53:09 +0200

    Change position of inserted status fields

--- dbv_data/revisions/1211/contest.statusflags.sql
+++ dbv_data/revisions/1211/contest.statusflags.sql
@@ -1,4 +1,4 @@
-ALTER TABLE  `contest` ADD  `visibility`  ENUM( 'Visible',  'Hidden' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  'Hidden';
+ALTER TABLE  `contest` ADD  `visibility`  ENUM( 'Visible',  'Hidden' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  'Hidden' AFTER `status`;
 
 UPDATE `contest` SET `startDate` = NOW(), `endDate` = NOW() + INTERVAL 3 YEAR WHERE `status` = 'RunningContest';
 UPDATE `contest` SET `startDate` = NOW() + INTERVAL 2 YEAR, `endDate` = NOW() + INTERVAL 3 YEAR WHERE `status` = 'FutureContest';
--- dbv_data/revisions/1212/contest.ranked.sql
+++ dbv_data/revisions/1212/contest.ranked.sql
@@ -1,1 +1,1 @@
-ALTER TABLE  `contest` ADD  `ranked`  ENUM( 'NotRanked',  'Ranked' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  'NotRanked';
+ALTER TABLE  `contest` ADD  `ranked`  ENUM( 'NotRanked',  'Ranked' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  'NotRanked'  AFTER `status`;
