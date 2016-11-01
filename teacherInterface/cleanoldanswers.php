e982144bfdcbf017582f52901759b0f9cc1259f6
Elie Roux <elie.roux@telecom-bretagne.eu>
2016-11-01 16:49:58 +0100

    add script to clean old answers

---
+++ teacherInterface/cleanoldanswers.php
@@ -0,0 +1,25 @@
+<?php 
+/* Copyright (c) 2012 Association France-ioi, MIT License http://opensource.org/licenses/MIT */
+
+require_once("../shared/common.php");
+require_once("commonAdmin.php");
+
+if (!isset($_SESSION["isAdmin"]) || !$_SESSION["isAdmin"]) {
+      echo json_encode((object)array("status" => 'error', "message" => "Seul un admin peut effectuer cette opération"));
+      exit;
+}
+
+$query = "delete team_question from team_question
+join team on team.ID = team_question.teamID
+join `group` on `group`.ID = team.groupID
+where `group`.isPublic = 1 and team.startTime < (now() - interval 1 week);";
+$stmt = $db->prepare($query);
+$stmt->execute();
+
+$query = "delete team from team
+join `group` on `group`.ID = team.groupID
+where `group`.isPublic = 1 and team.startTime < (now() - interval 1 week);";
+$stmt = $db->prepare($query);
+$stmt->execute();
+
+echo json_encode(['success' => true]);
