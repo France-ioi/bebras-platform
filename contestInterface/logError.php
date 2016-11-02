266ce429dcec17e7467fa3667579388867a846fb
Elie Roux <elie.roux@telecom-bretagne.eu>
2016-11-02 11:29:13 +0100

    use uap-parser

--- contestInterface/logError.php
+++ contestInterface/logError.php
@@ -5,15 +5,21 @@ if (!isset($_POST['errormsg'])) {
 }
 
 require_once("../shared/common.php");
+require_once("../vendor/autoload.php");
+
+use UAParser\Parser;
 
 initSession();
+
+$parser = Parser::create();
 
 $teamID = isset($_SESSION["teamID"]) ? $_SESSION["teamID"] : null;
 $questionKey = isset($_POST["questionKey"]) ? $_POST["questionKey"] : null;
 
 $errormsg = $_POST['errormsg'];
 
-$browserStr = 'Unknown-tmp';
+$browserStr = $parser->parse($_SERVER['HTTP_USER_AGENT']);
+$browserStr = $browserStr->toString();
 
 $stmt = $db->prepare('insert into error_log (date, teamID, message, browser, questionKey) values (UTC_TIMESTAMP(), :teamID, :errormsg, :browserStr, :questionKey);');
 $stmt->execute(['teamID' => $teamID, 'errormsg' => $errormsg, 'browserStr' => $browserStr, 'questionKey' => $questionKey]);
