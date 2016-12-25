deb6d3cb7fb1b4ef2eec8c7b8de38dad0ba350f1
Sebastien Carlier <s.carlier@epixode.fr>
2016-12-25 23:07:06 +0000

    breaking badgeInterface API change
    - the full path (without leading slash) is used as the badge name
    - the action must be passed as a POST param
    - Apache .htaccess updated to send all requests to index.php
    - compatible with nginx (simply use fastcgi_pass in location /
      and set SCRIPT_FILENAME to /.../index.php)

---
+++ badgeInterface/functions.php
@@ -0,0 +1,81 @@
+<?php
+
+require_once '../shared/connect.php';
+
+function exitWithJson($json) {
+   header("Content-Type: application/json");
+   header("Connection: close");
+   echo json_encode($json);
+   exit;
+}
+
+function exitWithJsonFailure($message) {
+   $result = array("success" => false, "error" => $message);
+   exitWithJson($result);
+}
+
+function getRequiredParam($key) {
+  if (!array_key_exists($key, $_POST)) {
+    exitWithJsonFailure($key.' param is required');
+  }
+  return $_POST[$key];
+}
+
+/* Return the user details for the matching (badgeName, code) pair, or null if
+   no match is found. */
+function verifyCode($badgeName, $code) {
+  $stmt = $db->prepare('select contestant.lastName as sLastName, contestant.firstName as sFirstName, contestant.genre as genre, contestant.email as sEmail, contestant.zipcode as sZipcode from contestant
+    join team on team.ID = contestant.teamID
+    join `group` on `group`.ID = team.groupID
+    join contest on contest.ID = `group`.contestID
+    where algoreaCode = :code and contest.badgeName = :badgeName;');
+  $stmt->execute(['code' => $code, 'badgeName' => $badgeName]);
+
+  $contestant = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  if (!$contestant) {
+    return null;
+  }
+
+  $contestant['sSex'] = ($contestant['genre'] == 2 ? 'Male' : 'Female');
+  unset($contestant['genre']);
+
+  return $contestant;
+}
+
+/* Associate the user with ID idUser to the given (badgeName, code) pair. */
+function updateAlgoreaRegistration($badgeName, $code, $idUser) {
+  $stmt = $db->prepare('select algorea_registration.* from algorea_registration
+    join contestant on contestant.ID = algorea_registration.contestantID
+    join team on team.ID = contestant.teamID
+    join `group` on `group`.ID = team.groupID
+    join contest on contest.ID = `group`.contestID
+    where algoreaCode = :code and contest.badgeName = :badgeName;');
+  $stmt->execute(['code' => $code, 'badgeName' => $badgeName]);
+
+  $infos = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  if ($infos) {
+    if ($infos['franceioiID'] != $idUser) {
+      return ['success' => false, 'error' => 'code is already registered by someone else'];
+    }
+    return ['success' => true];
+  }
+
+  $stmt = $db->prepare('select contestant.ID from contestant
+    join team on team.ID = contestant.teamID
+    join `group` on `group`.ID = team.groupID
+    join contest on contest.ID = `group`.contestID
+    where algoreaCode = :code and contest.badgeName = :badgeName;');
+  $stmt->execute(['code' => $code, 'badgeName' => $badgeName]);
+
+  $contestantID = $stmt->fetchColumn();
+
+  if (!$contestantID) {
+    return ['success' => false, 'error' => 'code is not valid'];
+  }
+
+  $stmt = $db->prepare('insert into algorea_registration (code, contestantID, franceioiID) values (:code, :contestantID, :franceioiID);');
+  $stmt->execute(['code' => $code, 'contestantID' => $contestantID, 'franceioiID' => $idUser]);
+  return ['success' => true];
+}
---
+++ badgeInterface/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require './functions.php';
+
+// Use the path (without leading slash) as the badge name.
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if (!is_string($path)) {
+  exitWithJsonFailure('invalid path');
+}
+$badgeName = ltrim($path, '/');
+
+// The action is passed as a POST param.
+$action = getRequiredParam('action');
+
+if ($action == 'verifyCode') {
+  $code = getRequiredParam('code');
+  exitWithJson(verifyCode($badgeName, $code));
+}
+
+if ($action == 'updateInfos') {
+  $code = getRequiredParam('code');
+  $idUser = getRequiredParam('idUser');
+  exitWithJson(updateAlgoreaRegistration($badgeName, $code, $idUser));
+}
+
+exitWithJsonFailure($action.' is not a valid action');
