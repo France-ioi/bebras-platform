c404619133bb848dd17793f98204f66c53938e34
Melanie Storup <m.storup@epixode.fr>
2017-12-26 16:13:21 +0100

    cleanup contest password

--- contestInterface/index.php
+++ contestInterface/index.php
@@ -400,14 +400,11 @@
 </div><!-- #divLogin -->
 
 <div id="divPassword" style="display:none" class="dialog">
-   <p data-i18n="[html]password_warning">
-   </p>
-   <p>
-   <span data-i18n="access_code"></span> <span id="teamPassword" class="selectable" style="font-size:2em"></span>
-   </p>
-         <div class="divInput">
-            <button type="button" data-i18n="password_confirm" id="buttonConfirmTeamPassword" onclick="confirmTeamPassword()"></button>
-         </div>
+  <p data-i18n="[html]password_warning"></p>
+  <p><span data-i18n="access_code"></span> <span id="teamPassword" class="selectable" style="font-size:2em"></span></p>
+  <div class="divInput">
+    <button type="button" data-i18n="password_confirm" id="buttonConfirmTeamPassword" onclick="confirmTeamPassword()" class="btn btn-default"></button>
+  </div>
 </div>
 </form>
 <div id="divImagesLoading" style="display:none" class="dialog">
