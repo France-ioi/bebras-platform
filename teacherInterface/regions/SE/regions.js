71a828a8fe69919728def8de5e2568cde651bdf2
Mathias Hiron <mathias.hiron@gmail.com>
2018-11-01 22:23:36 +0100

    sv translations and regions

--- teacherInterface/regions/SE/regions.js
+++ teacherInterface/regions/SE/regions.js
@@ -1,4 +1,24 @@
 regions = [
-   'countrySE:sweden',
-   'countrySE:foreign'
+   "countrySE:foreign",
+   "countrySE:stockholms",
+   "countrySE:uppsala",
+   "countrySE:sodermanlands",
+   "countrySE:ostergotlands",
+   "countrySE:jonkopings",
+   "countrySE:kronobergs",
+   "countrySE:kalmar",
+   "countrySE:gotlands",
+   "countrySE:blekinge",
+   "countrySE:skane",
+   "countrySE:hallands",
+   "countrySE:vastra",
+   "countrySE:varmlands",
+   "countrySE:orebro",
+   "countrySE:vastmanlands",
+   "countrySE:dalarnas",
+   "countrySE:gavleborgs",
+   "countrySE:vasternorrlands",
+   "countrySE:jamtlands",
+   "countrySE:vasterbottens",
+   "countrySE:norrbottens"
 ];
