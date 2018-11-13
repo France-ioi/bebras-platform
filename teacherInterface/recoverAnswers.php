085897cc9286d6d460079a333186ce1b566ea195
Mathias Hiron <mathias.hiron@gmail.com>
2018-11-13 14:13:45 +0100

    fix recoverAnswers

--- teacherInterface/recoverAnswers.php
+++ teacherInterface/recoverAnswers.php
@@ -12,12 +12,12 @@ $stmt->execute(array($pwd));
 if ($row = $stmt->fetchObject()) {
    $stmtInsert = $db->prepare("INSERT IGNORE INTO `team_question_recover` (`teamID`, `questionID`, `answer`) VALUES (?, ?, ?)");
    $stmtUpdate = $db->prepare("UPDATE `team_question_recover` SET `answer` = ? WHERE `teamID` = ? AND `questionID` = ?");
-   $stmtDelete = $db->prepare("UPDATE `team` SET `score` = NULL WHERE teamID = ?");
+   $stmtReset = $db->prepare("UPDATE `team` SET `score` = NULL WHERE ID = ?");
    $teamID = $row->ID;
    foreach ($ans as $answer) {
       $stmtInsert->execute(array($teamID, $answer[0], $answer[1]));
       $stmtUpdate->execute(array($answer[1], $teamID, $answer[0]));
-      $stmtReset=>execute(array($teamID));
+      $stmtReset->execute(array($teamID));
    }
    echo json_encode(array("success" => true, "message" => "Team ".$teamID.", ".count($ans)." réponses enregistrées"));
 } else {
