eef12a70c24a27d15f694ed69b1d995e0142d3ab
Michel Blockelet <blockelet@crans.org>
2019-01-09 01:59:38 +0100

    contestInterface: i18n on questionIframeLoading

--- contestInterface/index.php
+++ contestInterface/index.php
@@ -596,7 +596,7 @@ $browserIsMobile = $browser->isType('mobile', 'tablet', 'ereader');
 </div>
 
 <div id="question-iframe-container" style="display:none" autocomplete="off">
-   <div class="questionIframeLoading">Chargement en cours...</div>
+   <div class="questionIframeLoading" data-i18n="questions_loading"></div>
    <div class="newInterface questionIframeHeader">
       <span class="questionTitle" style="padding-right: 20px"></span><span id="questionIframeStars"></span>
    </div>
