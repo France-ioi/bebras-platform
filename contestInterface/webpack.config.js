c355c27953342e6fd90d2f5d6ad713f3e66dcd77
mobydimk <mobydimk@gmail.com>
2019-05-01 12:21:12 +0400

    webpack config bugfix

--- contestInterface/webpack.config.js
+++ contestInterface/webpack.config.js
@@ -3,50 +3,54 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 require('babel-register');
 const webpack = require('webpack');
 
-const isDev = process.env.NODE_ENV !== 'production';
-const isDebug = process.env.DEBUG === '1';
+
 
 // Webpack Configuration
-module.exports = {
-	// Entry
-	entry: ['./app/app.js'],
+module.exports = function() {
+	const isDev = process.env.NODE_ENV !== 'production';
+	const isDebug = process.env.NODE_ENV === 'debug';
 
-	// Output
-	output: {
-		path: path.resolve(__dirname, './build'),
-		filename: 'app.js',
-	},
-	externals: {
-		"jquery": "$"
-	},
-	// Loaders
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: "babel-loader"
-				  }
-			}
-		]
-	},
-	// Plugins
-	plugins: [
-		new webpack.DefinePlugin({
-			__DEBUG__: JSON.stringify(isDebug)
-		})
-	],
-	optimization: {
-		minimize: !isDev,
-		minimizer: [new UglifyJsPlugin()],
-	},
+	return {
+		// Entry
+		entry: ['./app/app.js'],
 
-	// OPTIONAL
-	// Reload On File Change
-	watch: isDev,
-	// Development Tools (Map Errors To Source File)
-	devtool: isDev ? 'source-map': false,
+		// Output
+		output: {
+			path: path.resolve(__dirname, './build'),
+			filename: 'app.js',
+		},
+		externals: {
+			"jquery": "$"
+		},
+		// Loaders
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /(node_modules|bower_components)/,
+					use: {
+						loader: "babel-loader"
+					}
+				}
+			]
+		},
+		// Plugins
+		plugins: [
+			new webpack.DefinePlugin({
+				__DEBUG__: JSON.stringify(isDebug)
+			})
+		],
+		optimization: {
+			minimize: !isDev,
+			minimizer: [new UglifyJsPlugin()],
+		},
 
-	mode: isDev ? 'development' : 'production'
-};
+		// OPTIONAL
+		// Reload On File Change
+		watch: isDev,
+		// Development Tools (Map Errors To Source File)
+		devtool: isDev ? 'source-map': false,
+
+		mode: isDev ? 'development' : 'production'
+	}
+}
