c6f2da4e47f1e3027b69c1539101ce499b600b38
Mathias Hiron <mathias.hiron@gmail.com>
2021-01-05 17:43:15 +0100

    regions for Senegal / Congo

--- teacherInterface/regions/SN/regions.js
+++ teacherInterface/regions/SN/regions.js
@@ -1,3 +1,17 @@
 regions = [
-   'countrySN:foreign'
+   'countrySN:foreign',
+   'countrySN:fdakar',
+   'countrySN:fziguinchor',
+   'countrySN:fdiourbel',
+   'countrySN:fsaint-louis',
+   'countrySN:ftambacounda',
+   'countrySN:fkaolack',
+   'countrySN:fthies',
+   'countrySN:flouga',
+   'countrySN:ffatick',
+   'countrySN:fkolda',
+   'countrySN:fmatam',
+   'countrySN:fkaffrine',
+   'countrySN:fkédougou',
+   'countrySN:fsedhiou'
 ];
