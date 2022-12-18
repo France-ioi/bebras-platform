e66cf807e1fa5d9090e70d382b7c312330728b94
Michel Blockelet <blockelet@crans.org>
2022-12-18 18:28:26 +0100

    config: Fix partnerLogos default values

--- config.php
+++ config.php
@@ -100,6 +100,8 @@ $config->certificates = (object) array();
 $config->certificates->webServiceUrl = 'http://castor-informatique.fr.localhost/certificates/';
 $config->certificates->allow = false;
 $config->certificates->confIndexForThisPlatform = 0; // index of the conf in certificates/ (you shouldn't need to change it)
+$config->certificates->partnerLogos = [];
+$config->certificates->partnerLogosWidths = null;
 
 $config->grades = [-1,4,5,6,16,7,17,8,18,9,19,10,13,11,14,12,15,20,-4];
 
