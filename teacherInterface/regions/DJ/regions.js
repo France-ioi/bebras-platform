bac0e10f7076f602c37d6a9050f593ae382ebde1
Mathias Hiron <mathias.hiron@gmail.com>
2023-03-31 20:08:15 +0200

    Add support for Djibouti

---
+++ teacherInterface/regions/DJ/regions.js
@@ -0,0 +1,9 @@
+regions = [
+   'countryDJ:foreign',
+   'countryDJ:djibouti',
+   'countryDJ:arta',
+   'countryDJ:ali-sabieh',
+   'countryDJ:dikhil',
+   'countryDJ:obock',
+   'countryDJ:tadjourah'
+];
