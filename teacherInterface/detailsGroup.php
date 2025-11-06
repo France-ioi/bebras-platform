9929b54a65f721c3c32b89dc410f1e5354109a11
Michel Blockelet <blockelet@crans.org>
2025-11-06 15:28:59 +0000

    Fix group access

--- teacherInterface/detailsGroup.php
+++ teacherInterface/detailsGroup.php
@@ -3,6 +3,9 @@
 require_once("../shared/common.php");
 require_once("commonAdmin.php");
 
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors', '1');
+
 if (!isset($_SESSION["userID"])) {
    echo translate("session_expired");
    exit;
@@ -14,7 +17,6 @@ if (!isset($_GET["groupID"])) {
 }
 
 $groupID = $_GET["groupID"];
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -37,7 +39,7 @@ tr:hover { background-color: #ffff99; }
 $query = "SELECT `group`.`startTime`, team.password, `team_question`.ffScore, team_question.score AS tqScore, team.score, ".
 "GROUP_CONCAT(CONCAT(firstName, ' ', lastName, ' ') SEPARATOR ', ') as contestants, ".
 "`group`.name as groupName, question.name as questionName, ".
-"`group`.ID as groupID, team.ID as teamID, question.ID as questionID ".
+"`group`.ID as groupID, `group`.userID as userID, team.ID as teamID, question.ID as questionID ".
 "FROM `group` ".
 "JOIN team ON team.groupID = `group`.ID ".
 "JOIN `contestant` ON `contestant`.teamID = team.ID ".
@@ -52,7 +54,12 @@ $stmt = $db->prepare($query);
 $stmt->execute(['groupID' => $groupID]);
 
 $groups = array();
+$accessOk = !!$_SESSION["isAdmin"];
 while ($row = $stmt->fetchObject()) {
+   if($row->groupID == $groupID && $row->userID == $_SESSION['userID']) {
+      $accessOk = true;
+   }
+
    if (!isset($groups[$row->groupID])) {
       $groups[$row->groupID] = array("name" => $row->groupName, "startTime" => $row->startTime, "teams" => array());
    }
@@ -65,6 +72,11 @@ while ($row = $stmt->fetchObject()) {
       $score = $row->ffScore;
    }
    $groupRef["teams"][$row->teamID]["questions"][$row->questionName] = $score;
+}
+
+if(!$accessOk) {
+   echo translate("grader_inexistent_group");
+   exit;
 }
 
 foreach ($groups as $group) {
